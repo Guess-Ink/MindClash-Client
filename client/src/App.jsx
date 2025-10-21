7b171701e6ba381e1956a077feadd36d55f2c972
Rahmad-Tech00 <mohamadrahmad00@gmail.com>
2025-10-21 21:15:31 +0700

    State management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,3 +7,13 @@ import "./App.css";
 const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:3000", {
   transports: ["websocket"],
 });
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const [user, setUser] = useState({ id: null, nickname: "", roomCode: "" });
+
+  function handleJoin(userData) {
+    setUser(userData);
+    setCurrentPage("game");
+  }
+}
